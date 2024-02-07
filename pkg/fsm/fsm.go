d46f681ccee2bc05a20237b3f600eae0a9131df5
Trygve <trygtaf@online.no>
2024-02-07 10:52:13 +0100

    added skeleton for FSM

--- pkg/fsm/fsm.go
+++ pkg/fsm/fsm.go
@@ -1,1 +1,24 @@
 package fsm
+
+
+
+import (
+	"OTP.com/Heis2e/pkg/elevio"
+	"OTP.com/Heis2e/pkg/timer"
+	"fmt"
+)
+
+func FSM(Button_ch chan elevio.ButtonEvent, Floor_sensor_ch chan int, Stop_button_ch chan bool, Obstruction_ch chan bool){
+	for {
+		select {
+		case Buttonevent := <-Button_ch:
+			
+		case Newfloor := <-Floor_sensor_ch:
+			
+		case Stopbutton := <-Stop_button_ch:
+			
+		case Obstruction := <-Obstruction_ch:
+			
+		}
+	}
+}
