1dd70dce9d97909a4b351e5baedd70d877c57e2a
Ole Thorrud <olethorrud@dhcp-10-24-35-97.wlan.ntnu.no>
2024-02-14 12:49:56 +0100

    Oles implementasjoner av deler av requests.c

--- pkg/elevator/requests.go
+++ pkg/elevator/requests.go
@@ -1,2 +1,48 @@
 package elevator
 
+func requests_above(e Elevator) (bool){
+	for flr := e.Floor + 1; flr < N_FLOORS; flr++ {
+		for btn := 0; flr < N_BUTTONS; btn++{
+			if e.Requests[flr][btn] == 1 {
+				return true; 
+			}	
+		}
+	}
+	return false; 
+}
+
+func requests_below(e Elevator) (bool){
+	for flr := 0; flr < e.Floor; flr++ {
+		for btn := 0; flr < N_BUTTONS; btn++{
+			if e.Requests[flr][btn] == 1 {
+				return true; 
+			}	
+		}
+	}
+	return false; 
+}
+
+func requests_here(e Elevator) (bool){
+	for btn := 0; btn < N_BUTTONS; btn++ {
+		if e.Requests[e.Floor][btn] == 1{
+			return true; 
+		}
+	}
+
+	return false; 
+}
+
+func requests_shouldStop(e Elevator) (bool){
+	switch e.Dirn{
+	case MD_Down: 
+		return (e.Requests[e.Floor][BT_HallDown] == 1 || 
+				e.Requests[e.Floor][BT_Cab] == 1|| 
+				!requests_below(e)); 
+	case MD_Up: 
+		return (e.Requests[e.Floor][BT_HallUp] == 1 ||
+				e.Requests[e.Floor][BT_Cab] == 1 ||
+				!requests_above(e)); 
+	default: 
+		return true
+	}
+}
